7199669edf2232724033d99486db55b3f154f08c
aalmondd <aalmonacidd@unal.edu.co>
2025-06-20 11:13:04 -0500

    Added .sh automatizing subtree on spectral index

--- push-sub.sh
+++ push-sub.sh
@@ -1,11 +1,11 @@
 
 #!/bin/bash
 DIRECTORY=~/FisUNAL/MIE/indice_espectral/ 
-BRANCH=Spectral_index
+BRANCH=Subtree
 REPO_URL=git@github.com:aalmonacidd/Spectral_index.git
 
 git subtree split -P $DIRECTORY -b $BRANCH
 git push $REPO_URL $BRANCH:main --force
 git branch -D $BRANCH
 
-echo "✅ Subida exitosa de '$DIRECTORY/' al repositorio $REPO_URL"
+
